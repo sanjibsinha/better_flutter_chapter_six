f54461644be5e2a62fde28e44982e90ee61858f8
Sanjib <sanjib12sinha@gmail.com>
2021-06-18 07:13:21 +0530

    fifth

--- lib/chapter_six/test_category/test_category_item.dart
+++ lib/chapter_six/test_category/test_category_item.dart
@@ -9,23 +9,50 @@ class TestCategoryItem extends StatelessWidget {
     required this.color,
   }) : super(key: key);
 
+  void selectCategory(BuildContext context) {
+    Navigator.push(context, MaterialPageRoute(builder: (_) {
+      return CategoryBooks();
+    }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.center,
-      margin: const EdgeInsets.all(
-        10.0,
+    return InkWell(
+      onTap: () => selectCategory(context),
+      splashColor: Theme.of(context).primaryColor,
+      borderRadius: BorderRadius.circular(15),
+      child: Container(
+        alignment: Alignment.center,
+        margin: const EdgeInsets.all(
+          10.0,
+        ),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline5,
+        ),
+        decoration: BoxDecoration(
+            color: color,
+            border: Border.all(
+              width: 5,
+              color: Colors.black26,
+            )),
       ),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline5,
+    );
+  }
+}
+
+class CategoryBooks extends StatelessWidget {
+  const CategoryBooks({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Books'),
       ),
-      decoration: BoxDecoration(
-          color: color,
-          border: Border.all(
-            width: 5,
-            color: Colors.black26,
-          )),
+      body: Center(
+        child: Text('Books'),
+      ),
     );
   }
 }
